abf2a7214475d8fc7b30f2844677327733162a19
sofiamatulis <sosomatulis@hotmail.com>
2016-09-14 15:41:24 -0400

    created check winner option and added class

--- ttt.js
+++ ttt.js
@@ -5,16 +5,25 @@
 $(function(){
   var turn = 0;
 
+
   $('.square').on('click', function() {
 
+
+
     var self = $(this);
+
+    // how you store a state:
+
+    // self.data('position');
 
 //this calls on the square. dom element in which the event occured!!
    if ( self.text() === ' ' ) {
      if ( turn % 2 === 0 ){
        self.text('X');
+         self.addClass("x");
      } else {
         self.text('O');
+          self.addClass("o");
     }
 
     turn++;
@@ -23,5 +32,18 @@ $(function(){
   }
 
   });
+
+  $('.square').click ( function() {checkWinner() });
+
+      var self = $(this);
+
+      function checkWinner(){
+
+      }
+
+
+
+
+
 
 });
