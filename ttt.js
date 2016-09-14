2a625a8c7b1e63448194f4df86d3418aaf265033
sofiamatulis <sosomatulis@hotmail.com>
2016-09-14 11:22:20 -0400

    the app wont allow something to replace what was already written

--- ttt.js
+++ ttt.js
@@ -1,26 +1,26 @@
 'use strict'
 
-
 //ensure doc is ready
 
 $(function(){
   var turn = 0;
 
-  $('.square').on('click',function() {
+  $('.square').on('click', function() {
+
+    var self = $(this);
 
 //this calls on the square. dom element in which the event occured!!
-    if (turn % 2 ===0){
-    $(this).text('X'); }
-    else {
-        $(this).text('O');
-
+   if ( self.text() === ' ' ) {
+     if ( turn % 2 === 0 ){
+       self.text('X');
+     } else {
+        self.text('O');
     }
 
     turn++;
-
-  
-
-
+  } else {
+    alert("I'm sorry");
+  }
 
   });
 
