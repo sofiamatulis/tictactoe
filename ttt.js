0efb0585c6190963112a8f81f197205493b13809
sofiamatulis <sosomatulis@hotmail.com>
2016-09-14 11:02:04 -0400

    created turn variable

--- ttt.js
+++ ttt.js
@@ -1,7 +1,8 @@
 //ensure doc is ready
 
-$(function()
-{
+$(function(){
+  var turn = 0;
+
   $('.square').on('click',function() {
 
 //this calls on the square. dom element in which the event occured!!
